5040e3733ccd9f0e48065e5b4c3c343ba0a8930e
madhav-datt <madhav.datt@hotmail.com>
2016-04-28 17:56:34 +0530

    add bug fixes

--- submodules.v
+++ submodules.v
@@ -1,30 +1,19 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Author:				Aakash Verma (14CS30001), Madhav Datt (14CS30015) 
-// 
-// Create Date:    	14:39:20 03/28/2016 
-// Design Name: 
-// Module Name:    	submodules 
-// Project Name: 		LFSR
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: Submodules D Flip-Flop, Multiplexer, Clock Dividor
-//
-//////////////////////////////////////////////////////////////////////////////////
 
-/*
+/**
+ * Madhav Datt
+ *
+ * MIT License
+ * Submodules D Flip-Flop, Multiplexer, Clock Dividor
+ */
+
+/**
  * D Fli-Flop Module
  */
 module DFF (output reg out,
-				input in, clk, rst
-				);
-					
+			input in, clk, rst
+			);
+
 	always @ (posedge clk or posedge rst)
 	begin
 		if (rst == 1'b1)
@@ -32,50 +21,46 @@ module DFF (output reg out,
 		else
 			out <= in;
 	end
-	
+
 endmodule
 
-/*
+/**
  * Multiplexer Module
  * out = a * ~sel + b * sel
  */
-module MUX (output reg out, 
-				input a,b,sel
-				);
-			
+module MUX (output reg out,
+			input a,b,sel
+		    );
+
 	always @ (sel or a or b)
-	begin 
+	begin
 		if (sel == 1'b1)
 			out = b;
-		else 
+		else
 			out = a;
 	end
-	
+
 endmodule
 
-/*
+/**
  * Clock Dividor
  * Slows clock speed by a factor of 2^20 (~10^6)
  * For 4 MHz clock of FPGA, output will be ~4 Hz
  */
 module CLKDiv (output clk_div,
-					input clk, rst
-					);
-	
+			   input clk, rst
+			   );
+
 	reg [19:0] outclk;
-	
-	always @ (posedge clk or posedge rst)   
+
+	always @ (posedge clk or posedge rst)
 	begin
 		 if (rst == 1'b1)
 			   outclk <= 20'b0;
 		 else
 			  outclk <= outclk + 1'b1;
-			 
-		// Give MSB of 20-bit counter
-		 		  
 	end
+
+    // Give MSB of 20-bit counter
 	assign clk_div = outclk[19];
 endmodule
-					
-	
-		
--- toplevel.v
+++ toplevel.v
@@ -1,32 +1,20 @@
 `timescale 1ns / 1ps
 
-//////////////////////////////////////////////////////////////////////////////////
-// Author:				Aakash Verma (14CS30001), Madhav Datt (14CS30015) 
-// 
-// Create Date:    	15:02:41 03/28/2016 
-// Design Name: 
-// Module Name:    	toplevel 
-// Project Name: 		LFSR
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: Linear Feedback Shift Registers
-//
-//////////////////////////////////////////////////////////////////////////////////
+/**
+ * Madhav Datt
+ *
+ * MIT License
+ * Linear Feedback Shift Registers
+ */
 
-/*
+/**
  * Top Level Module for Linear Feedback Shift Registers
  */
-module LFSR (output [3:0] state, 
+module LFSR (output [3:0] state,
 				 input [3:0] seed,
 				 input rst, sel, clk
 				 );
-	
+
 	wire [3:0] in_w;
 	wire [4:0] w;
 	wire clock_div;
@@ -37,26 +25,26 @@ module LFSR (output [3:0] state,
 	assign state[2] = w[2];
 	assign state[1] = w[3];
 	assign state[0] = w[4];
-	
+
 	// Multiplexers to generate DFF input with seed and w
 	// sel is the select value
 	MUX m3 (in_w[0], seed[3], w[0], sel),
 		 m2 (in_w[1], seed[2], w[1], sel),
 		 m1 (in_w[2], seed[1], w[2], sel),
 		 m0 (in_w[3], seed[0], w[3], sel);
-	
+
 	// Clock dividor, gives ~4 Hz frequency
 	CLKDiv c_div (clock_div, clk, rst);
-	
+
 	// D Flip-Flops to give values to w
 	DFF d3 (w[1], in_w[0], clock_div, rst),
 		 d2 (w[2], in_w[1], clock_div, rst),
 		 d1 (w[3], in_w[2], clock_div, rst),
 		 d0 (w[4], in_w[3], clock_div, rst);
-		 
+
 	// Feedback to w[0] as w[4] xor w[3]
 	assign w[0] = w[4] ^ w[3];
-	
+
 endmodule
 
 
@@ -64,11 +52,11 @@ endmodule
  * Top Level Module Augmented for Linear Feedback Shift Registers
  * With 0000 state
  */
-module LFSR_Aug (output [3:0] state, 
+module LFSR_Aug (output [3:0] state,
 						input [3:0] seed,
 						input rst, sel, clk
 						);
-	
+
 	wire [3:0] in_w;
 	wire [4:0] w;
 	wire clock_div;
@@ -81,14 +69,14 @@ module LFSR_Aug (output [3:0] state,
 	assign state[2] = w[2];
 	assign state[1] = w[3];
 	assign state[0] = w[4];
-	
+
 	// Multiplexers to generate DFF input with seed and w
 	// sel is the select value
 	MUX m3 (in_w[0], seed[3], w[0], sel),
 		 m2 (in_w[1], seed[2], w[1], sel),
 		 m1 (in_w[2], seed[1], w[2], sel),
 		 m0 (in_w[3], seed[0], w[3], sel);
-	
+
 	// Clock dividor, gives ~4 Hz frequency
 	CLKDiv c_div (clock_div, clk, rst);
 
@@ -97,10 +85,10 @@ module LFSR_Aug (output [3:0] state,
 		 d2 (w[2], in_w[1], clock_div, rst),
 		 d1 (w[3], in_w[2], clock_div, rst),
 		 d0 (w[4], in_w[3], clock_div, rst);
-		 
+
 	// Feedback to w[0]
 	assign nor_out = ~(w[1] | w[2] | w[3]);
 	assign xor_out = nor_out^w[4];
 	assign w[0] = (xor_out^w[1]);
-	
+
 endmodule
